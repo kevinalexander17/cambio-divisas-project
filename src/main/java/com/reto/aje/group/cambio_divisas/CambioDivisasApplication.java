c03eed8b5c68a74468040c107f1f13aa28df5d2b
Kevin Alexander Quispe Roman <qalexander370@gmail.com>
2025-01-22 18:35:26 -0500

    Iniciando reto técnico Aje group

---
+++ src/main/java/com/reto/aje/group/cambio_divisas/CambioDivisasApplication.java
@@ -0,0 +1,13 @@
+package com.reto.aje.group.cambio_divisas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambioDivisasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambioDivisasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto/aje/group/cambio_divisas/CambioDivisasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto.aje.group.cambio_divisas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioDivisasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
